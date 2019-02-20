064483016a40ba7b851d521d1438f5dbd728186e
GusNitrous <gusnitrous@gmail.com>
2019-02-20 23:47:07 +0300

    Navigation. Manage products page.

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/material.dart';
 import '../product_manager.dart';
+import './manage_products.dart';
 
 class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('RealWorldApp'),
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              automaticallyImplyLeading: false,
+              title: Text('Choose'),
+            ),
+            ListTile(
+              title: Text('Manage Products'),
+              onTap: () {
+                print('tap on list tile');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (BuildContext context) => ManageProductsPage(),
+                  ),
+                );
+              },
+            ),
+          ],
         ),
-        body: ProductManager(),
-      );
+      ),
+      appBar: AppBar(
+        title: Text('RealWorldApp'),
+      ),
+      body: ProductManager(),
+    );
   }
-
 }
