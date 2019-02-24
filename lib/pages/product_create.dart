6d247ddfc6ae7777ab538c97742afaff004ecc50
GusNitrous <gusnitrous@gmail.com>
2019-02-24 15:22:14 +0300

    User input. Basic text fields.

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,21 +1,49 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget {
+class ProductCreatePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductPageState();
+  }
+}
+
+class _ProductPageState extends State<ProductCreatePage> {
+  String titleValue = '';
+  double priceValue;
+  String descriptionValue = '';
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        child: Text('Save'),
-        onPressed: () {
-          showModalBottomSheet(
-              context: context,
-              builder: (BuildContext context) {
-                return Center(
-                  child: Text('This is a Modal!'),
-                );
-              });
-        },
-      ),
-    );
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: Column(
+      children: <Widget>[
+        TextField(
+          decoration: InputDecoration(labelText: 'Title'),
+          onChanged: (String value) {
+          setState(() {
+            titleValue = value;
+          });
+        },),
+
+        TextField(
+          decoration: InputDecoration(labelText: 'Price'),
+          keyboardType: TextInputType.number,
+          onChanged: (String value) {
+          setState(() {
+            priceValue = double.parse(value);
+          });
+        },),
+
+        TextField(
+          decoration: InputDecoration(labelText: 'Description'),
+          maxLines: 4,
+          onChanged: (String value) {
+          setState(() {
+            descriptionValue = value;
+          });
+        },),
+      ],
+    ));
   }
 }
