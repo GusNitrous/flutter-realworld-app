7af6ff9b539d89d6d17b4c3ea62796958ed234b3
GusNitrous <gusnitrous@gmail.com>
2019-03-13 22:55:11 +0300

    Widget deep dive. Added font.

--- lib/products.dart
+++ lib/products.dart
@@ -12,7 +12,10 @@ class Products extends StatelessWidget {
       child: Column(
         children: <Widget>[
           Image.asset(products[index]['image']),
-          Text(products[index]['title']),
+          Container(
+            padding: EdgeInsets.only(top: 10.0),
+            child: Text(products[index]['title'], style: TextStyle(fontSize: 26.0, fontFamily: 'Oswald', fontWeight: FontWeight.bold),),
+          ),
           ButtonBar(
             alignment: MainAxisAlignment.center,
             children: <Widget>[
